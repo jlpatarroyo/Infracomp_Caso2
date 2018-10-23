bf9b4f9e1a943544f61ad0f0792dbbfc93301791
Jose Patarroyo <jl.patarroyo@uniandes.edu.co>
2018-10-23 08:57:19 -0500

    Creación del proyecto

---
+++ src/ServidorPrincipal.java
@@ -0,0 +1,7 @@
+
+public class ServidorPrincipal {
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/ServidorThread.java
@@ -0,0 +1,14 @@
+
+public class ServidorThread extends Thread{
+	
+	public ServidorThread() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
+
+}
